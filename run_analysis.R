e25718f0c11d43f23ae728c682a835b386457e93
Sergey Lebedev <sergey.lebedev@inbox.ru>
2015-06-22 01:49:16 +0300

    codebook added

--- run_analysis.R
+++ run_analysis.R
@@ -5,8 +5,8 @@ setwd('UCI HAR Dataset')
 activity_labels <- fread('activity_labels.txt')
 features <- fread('features.txt')
 # selected features
-selected_features_id <- features[grepl('-mean\\(\\)$', features[[2]])][[1]]
-selected_features_id <- append(selected_features_id, features[grepl('-std\\(\\)$', features[[2]])][[1]])
+selected_features_id <- features[grepl('-mean\\(\\)', features[[2]])][[1]]
+selected_features_id <- append(selected_features_id, features[grepl('-std\\(\\)', features[[2]])][[1]])
 selected_features_id <- sort(selected_features_id)
 selected_features <- features[selected_features_id][[2]]
 # reading raw train data
@@ -18,7 +18,7 @@ raw_train <- t(raw_train)
 subject_train <- fread('subject_train.txt')
 y_train <- fread('y_train.txt')
 setwd('..')
-# tyding train data
+# tidying train data
 activity_type <- sapply(y_train, function(x){return(activity_labels[x][[2]])})
 train <- data.frame(subject_train, activity_type)
 colnames(train) <- c('subject_id', 'activity_type')
@@ -35,7 +35,7 @@ raw_test <- t(raw_test)
 subject_test <- fread('subject_test.txt')
 y_test <- fread('y_test.txt')
 setwd('..')
-# tyding test data
+# tidying test data
 activity_type <- sapply(y_test, function(x){return(activity_labels[x][[2]])})
 test <- data.frame(subject_test, activity_type)
 colnames(test) <- c('subject_id', 'activity_type')
@@ -43,10 +43,10 @@ for(selected_feature_id in selected_features_id){
     feature_name <- features[selected_feature_id][[2]]
     test[[feature_name]] <- raw_test[,selected_feature_id]
 }
-# merging tyde train and test data
+# merging tidy train and test data
 data <- merge(train, test, all=TRUE)
-# getting new data
 melted <- melt(data, id=c('subject_id', 'activity_type'), measure.vars=selected_features)
+# getting new data
 result <- dcast(melted, activity_type + subject_id ~ variable, mean)
 # writing result
 setwd('..')
